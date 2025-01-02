e864b26e56a09486e56bd108085a42a2e9774e44
ranadeep4 <dema.ranadeepreddy2004@gmail.com>
2025-01-02 22:40:11 +0530

    adding backend folder

---
+++ Backend/app.js
@@ -0,0 +1,18 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+
+// app.listen(3000,()=>{
+//     console.log('Server is running on port 3000');
+// })
+
+module.exports=app
